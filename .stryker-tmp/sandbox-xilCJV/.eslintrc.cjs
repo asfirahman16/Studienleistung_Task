c45491f15f70f2b7300402015007b302db96c074
Asfi rahman <asfirahman0802@gmail.com>
2025-12-31 19:34:22 +0100

    4th commit

--- .stryker-tmp/sandbox-xilCJV/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-module.exports = {
-    env: { node: true, es2022: true, jest: true },
-    extends: ["eslint:recommended", "plugin:sonarjs/recommended"],
-    plugins: ["sonarjs"],
-    rules: {
-        "no-console": "off"
-    }
-};
--- .stryker-tmp/sandbox-xilCJV/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-module.exports = {
-    testEnvironment: "node",
-    collectCoverage: true,
-    coverageDirectory: "artifacts/coverage",
-    coverageReporters: ["text", "lcov"],
-};
